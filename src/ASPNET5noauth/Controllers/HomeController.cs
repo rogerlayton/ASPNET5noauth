09e418abac977abd8d00dfdbacfcc2958e32fb9c
Roger Layton <roger@rl.co.za>
2016-01-24 15:55:18 +0200

    First commit

---
+++ src/ASPNET5noauth/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using ASPNET5noauth.Models;
+
+namespace ASPNET5noauth.Controllers
+{
+
+
+
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Foo()
+        {
+            ViewData["Message"] = "This is the message for FOO.";
+            ViewData["FooMsg"] = "Foo Message";
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            var p = new Person()
+            {
+                FirstName = "Roger",
+                LastName = "Layton",
+                Birthdate = new DateTime(1953, 3, 19)
+            };
+
+            return View(p);
+        }
+
+        [HttpPost]
+        public IActionResult About(Person personIn)
+        {
+            return View(personIn);
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/ASPNET5noauth/Models/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNET5noauth.Models
+{
+    public class Person
+    {
+        [Required]
+        [StringLength(10, MinimumLength = 2)]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(10, MinimumLength = 2)]
+        public string LastName { get; set; }
+        [Required]
+        public DateTime Birthdate { get; set; }
+    }
+}
